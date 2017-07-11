a604645cc394e8fea0b168b490be113b790a2ef6
hunterhug <gdccmcm14@live.com>
2017-07-11 15:41:55 +0800

    ko!

--- static/js/toc.js
+++ static/js/toc.js
@@ -3,7 +3,7 @@
   $.fn.toc = function(options) {
     var defaults = {
       noBackToTopLinks: false,
-      title: '<i>搬砖的陈大师</i>',
+      title: '<i>萌飞の家</i>',
       minimumHeaders: 3,
       headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ol', // values: [ol|ul]
